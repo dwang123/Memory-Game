c0eadf06337b2a6957f7407db2784f0fc3f94452
dwang123 <derek.wang@greenwichschools.org>
2017-06-12 10:58:25 -0400

    Add files via upload

---
+++ MOMemoryGenerator.js
@@ -0,0 +1,35 @@
+var BoardList = {
+    
+    card00: [1,-1],
+    card01: [1,-1],
+    card02: [1,-1],
+    card03: [1,-1],
+    card04: [1,-1],
+    card05: [1,-1],
+    card10: [1,-1],
+    card11: [1,-1],
+    card12: [1,-1],
+    card13: [1,-1],
+    card14: [1,-1],
+    card15: [1,-1],
+    card20: [1,-1],
+    card21: [1,-1],
+    card22: [1,-1],
+    card23: [1,-1],
+    card24: [1,-1],
+    card25: [1,-1],
+    card30: [1,-1],
+    card31: [1,-1],
+    card32: [1,-1],
+    card33: [1,-1],
+    card34: [1,-1],
+    card35: [1,-1],
+};
+var Memestirdng =
+
+
+//all of the cards: 1 is flipped/unflipped, -1 is value of card
+function displayCards(){
+    BoardList.card+randomNumber(0,35);
+    
+}
---
+++ memoryGameDX.js
@@ -0,0 +1,12 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max + 1 - min)) + min; //Since Math.random() goes between 0 and 1, I
+                                                            //edit it to go between min and max, inclusive.
+}
+
+function setBoardFirstHalf(){
+var numbArray = [0,1,2,3,4,5,6,7,8,9,10,11]; //This array keeps track of which numbers have been used, so that
+                                             //we can have exactly two of each card.
+                                             //This also seems important for both games - maybe put in big doc pls?
+var i1 = getRandomInt(0,11);
+boardList.card0_0[]
+}
